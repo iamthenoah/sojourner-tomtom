649ab475663a5ba6aa67ac5537a6e4798b29db89
Noah <greffnoah@gmail.com>
2021-09-05 21:59:14 +0200

    separated url construction into its own function

--- ios/PointOfIntrestAPI.m
+++ ios/PointOfIntrestAPI.m
@@ -20,9 +20,46 @@ RCT_EXPORT_MODULE(TomTomAPI);
  */
 RCT_EXPORT_METHOD(fetchPOI:(NSString *)search paramaters:(NSDictionary *)additionalQueryParamaters resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
+  // build url with search term and params.
+  NSURL *url = [self constructURL:search params:additionalQueryParamaters];
+
+  // create request with url and session for sending req.
+  NSURLRequest *request = [NSURLRequest requestWithURL:url];
+  NSURLSession *session = [NSURLSession sharedSession];
+  
+  // perform request
+  NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+    
+    if (!error) {
+      NSError *parsingError = nil;
+      // attempt to parse JSON
+      NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&parsingError];
+      
+      if (parsingError) {
+        reject(@"parsing_failure", @"An error occured while fetch data.", error);
+      }
+      else {
+        resolve(json);
+      }
+    }
+    else {
+      reject(@"request_failure", @"Invalid search query provided.", error);
+    }
+  }];
+  
+  [task resume];
+}
+
+/**
+ * Handles building the URL with search terms and query paramaters
+ * @Param:query Search term.
+ * @Param:params Query paramaters.
+ */
+- (NSURL *) constructURL: (NSString *)query params:(NSDictionary *)params
+{
   NSString *endpoint = @"https://api.tomtom.com/search/2/poiSearch/";
   // append search query
-  endpoint = [endpoint stringByAppendingFormat:@"%@.json", search];
+  endpoint = [endpoint stringByAppendingFormat:@"%@.json", query];
 
   // create a key-value array for URL query paramaters,
   NSMutableArray *queryItems = [NSMutableArray array];
@@ -31,12 +68,10 @@ RCT_EXPORT_METHOD(fetchPOI:(NSString *)search paramaters:(NSDictionary *)additio
   // NOTE: Would not normally hard-code API key; kept it simple for the sake of this app.
   [queryItems addObject:[NSURLQueryItem queryItemWithName:@"key" value:@"xbut0FprHUpkK7BOoLxLzPYg6mDGOWyA"]];
   
-  if ([additionalQueryParamaters count])
-  {
+  if ([params count]) {
     // if has query params, cycle through and append to url component
-    for (NSString *key in additionalQueryParamaters)
-    {
-      [queryItems addObject:[NSURLQueryItem queryItemWithName:key value:additionalQueryParamaters[key]]];
+    for (NSString *key in params) {
+      [queryItems addObject:[NSURLQueryItem queryItemWithName:key value:params[key]]];
     }
   }
   
@@ -46,42 +81,16 @@ RCT_EXPORT_METHOD(fetchPOI:(NSString *)search paramaters:(NSDictionary *)additio
   // construct URL object.
   NSURLComponents *components = [NSURLComponents componentsWithString:endpoint];
   components.queryItems = queryItems;
-
-  // create request with url and session for sending req.
-  NSURLRequest *request = [NSURLRequest requestWithURL:components.URL];
-  NSURLSession *session = [NSURLSession sharedSession];
   
-  // perform request
-  NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error)
-  {
-    if (!error)
-    {
-      NSError *parsingError = nil;
-      // attempt to parse JSON
-      NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&parsingError];
-      
-      if (parsingError)
-      {
-        reject(@"parsing_failure", @"An error occured while fetch data.", error);
-      }
-      else
-      {
-        resolve(json);
-      }
-    }
-    else
-    {
-      reject(@"request_failure", @"Invalid search query provided.", error);
-    }
-  }];
-  
-  [task resume];
+  return components.URL;
 }
 
 /**
  * Used to append user coordinates to URL when fetching from the API.
+ * @Param:queryParamaters query paramaters.
  */
-- (void) appendUserCoordinates: (NSMutableArray*) queryParamaters {
+- (void) appendUserCoordinates: (NSMutableArray*) queryParamaters
+{
   CLLocationManager *locationManager = [[CLLocationManager alloc] init];
   locationManager.delegate = self;
   locationManager.distanceFilter = kCLDistanceFilterNone;
